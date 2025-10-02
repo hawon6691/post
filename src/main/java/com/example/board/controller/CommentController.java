af6135c34f0723a113d131839ead807a4f9bf4f5
hawon6691 <hawonoj002@gmail.com>
2025-10-03 02:56:25 +0900

    CommentController update #0

--- src/main/java/com/example/board/controller/CommentController.java
+++ src/main/java/com/example/board/controller/CommentController.java
@@ -1,11 +1,19 @@
 package com.example.board.controller;
 
 import com.example.board.service.CommentService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @GetMapping("/detali")
+    public String list(HttpSession session, Model model) {
+        return null;
+    }
 }
